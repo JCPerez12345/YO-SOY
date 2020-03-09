83b76f186579a7fb55ca34bc4f341694e7aa1c5c
JuanCa <charly.perez@hotmail.com>
2020-03-09 14:16:04 -0600

    Initial Commit

---
+++ YO SOY/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YO SOY
+//
+//  Created by Juan Carlos Perez Limon on 09/03/20.
+//  Copyright © 2020 JuanCa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
